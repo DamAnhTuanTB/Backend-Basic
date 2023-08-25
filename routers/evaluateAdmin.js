571ea1c200aae2cb7aa9ab6a084cfacb8b063b99
Anh Tuan (TuanDA) <tuanda@apero.vn>
2023-08-25 18:52:43 +0700

    Fix bug

--- routers/evaluateAdmin.js
+++ routers/evaluateAdmin.js
@@ -7,6 +7,28 @@ const router = express.Router();
 
 // router.use(auth);
 
+router.get("/", async (req, res) => {
+  try {
+    const results = await EvaluateModel.aggregate([
+      {
+        $group: {
+          _id: "$product", // Nhóm theo trường product (id sản phẩm)
+          totalComments: { $sum: 1 }, // Tổng số lượng đánh giá
+          averageStars: { $avg: "$numberStar" }, // Trung bình cộng số sao
+          star1: { $sum: { $cond: [{ $eq: ["$numberStar", 1] }, 1, 0] } },
+          star2: { $sum: { $cond: [{ $eq: ["$numberStar", 2] }, 1, 0] } },
+          star3: { $sum: { $cond: [{ $eq: ["$numberStar", 3] }, 1, 0] } },
+          star4: { $sum: { $cond: [{ $eq: ["$numberStar", 4] }, 1, 0] } },
+          star5: { $sum: { $cond: [{ $eq: ["$numberStar", 5] }, 1, 0] } }
+        }
+      }
+    ]);
+    console.log(results); // Mảng chứa thông tin thống kê cho mỗi sản phẩm
+  } catch (err) {
+    console.error(err);
+  }
+})
+
 router.get("/:id", async (req, res) => {
   try {
     let idProduct = req.params.id;
@@ -60,5 +82,16 @@ router.delete("/:id", (req, res) => {
         res.status(200).send({ message: "Xóa đánh giá thành công." })} )
     .catch((err) => res.status(500).send({ message: "Xóa đánh giá thất bại." }));
 });
+
+router.put("/allow-visible", async (req, res) => {
+  try{
+    const ids = req.body.idArr;
+    await EvaluateModel.updateMany({_id: {$in: ids}}, {allowVisible: true});
+    await EvaluateModel.updateMany({_id: {$nin: ids}}, {allowVisible: false});
+    res.status(200).send({ message: "Cập nhật các đánh giá sẽ được xuất hiện thành công." }) 
+  }catch(err){
+    res.status(500).send({ message: "Cập nhật các đánh giá sẽ được xuất hiện thất bại." }) 
+  }
+})
 
 module.exports = router;
