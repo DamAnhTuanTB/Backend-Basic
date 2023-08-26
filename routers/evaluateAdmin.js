f55309a637d743eea090f3e3744bbdb808b9921b
Anh Tuan (TuanDA) <tuanda@apero.vn>
2023-08-26 09:58:36 +0700

    Fix

--- routers/evaluateAdmin.js
+++ routers/evaluateAdmin.js
@@ -3,29 +3,112 @@ const express = require("express");
 
 const EvaluateModel = require("../models/evaluate");
 
+const ProductModel = require('../models/product')
+
 const router = express.Router();
 
 // router.use(auth);
 
 router.get("/", async (req, res) => {
+  let {keyword, page, limit } = req.params;
+
+  keyword = keyword || '';
+
+  const myPage = page || 1;
+
+  const myLimit = limit || 10;
   try {
-    const results = await EvaluateModel.aggregate([
+    //  const totalEvaluate = await BrandModel.countDocuments(queryBrand);
+    const result = await EvaluateModel.aggregate([
+  // Lookup và Unwind đầu tiên để áp dụng match
+  {
+    $lookup: {
+      from: ProductModel.collection.name,
+      localField: "product",
+      foreignField: "_id",
+      as: "productDetails"
+    }
+  },
+  {
+    $unwind: "$productDetails"
+  },
+  // Match sản phẩm bằng tên
+  {
+    $match: {
+      "productDetails.name": new RegExp(keyword, "i")
+    }
+  },
+  // Group
+  {
+    $group: {
+      _id: "$product",
+      totalReviews: { $sum: 1 },
+      averageStars: { $avg: "$numberStar" },
+      star1: { $sum: { $cond: [{ $eq: ["$numberStar", 1] }, 1, 0] } },
+      star2: { $sum: { $cond: [{ $eq: ["$numberStar", 2] }, 1, 0] } },
+      star3: { $sum: { $cond: [{ $eq: ["$numberStar", 3] }, 1, 0] } },
+      star4: { $sum: { $cond: [{ $eq: ["$numberStar", 4] }, 1, 0] } },
+      star5: { $sum: { $cond: [{ $eq: ["$numberStar", 5] }, 1, 0] } }
+      // ... (các field khác)
+    }
+  },
+  // Phân trang
+  {
+    $skip: (myPage - 1) * myLimit
+  },
+  {
+    $limit: myLimit
+  },
+  // Thêm lại Lookup và Unwind để có thông tin sản phẩm
+  {
+    $lookup: {
+      from: ProductModel.collection.name,
+      localField: "_id",
+      foreignField: "_id",
+      as: "productDetails"
+    }
+  },
+  {
+    $unwind: "$productDetails"
+  },
+  // Project
+  {
+    $project: {
+      productId: "$productDetails._id",
+      productName: "$productDetails.name",
+      totalReviews: 1,
+      averageStars: 1,
+      star1: 1,
+      star2: 1,
+      star3: 1,
+      star4: 1,
+      star5: 1,
+    }
+  }
+    ]);
+
+    const totalUniqueProducts = await EvaluateModel.aggregate([
       {
         $group: {
-          _id: "$product", // Nhóm theo trường product (id sản phẩm)
-          totalComments: { $sum: 1 }, // Tổng số lượng đánh giá
-          averageStars: { $avg: "$numberStar" }, // Trung bình cộng số sao
-          star1: { $sum: { $cond: [{ $eq: ["$numberStar", 1] }, 1, 0] } },
-          star2: { $sum: { $cond: [{ $eq: ["$numberStar", 2] }, 1, 0] } },
-          star3: { $sum: { $cond: [{ $eq: ["$numberStar", 3] }, 1, 0] } },
-          star4: { $sum: { $cond: [{ $eq: ["$numberStar", 4] }, 1, 0] } },
-          star5: { $sum: { $cond: [{ $eq: ["$numberStar", 5] }, 1, 0] } }
-        }
-      }
+          _id: "$product",  // Nhóm các đánh giá lại theo trường 'product' (sản phẩm)
+        },
+      },
+      {
+        $count: "totalUniqueProducts",  // Đếm số lượng nhóm đã được tạo, chính là số sản phẩm duy nhất
+      },
     ]);
-    console.log(results); // Mảng chứa thông tin thống kê cho mỗi sản phẩm
+    
+    const count = totalUniqueProducts.length ? totalUniqueProducts[0].totalUniqueProducts : 0;
+    res.status(200).send({
+        data: result,
+        totalProducts: count,
+        page: Number(myPage),
+        limit: myLimit,
+      });
   } catch (err) {
-    console.error(err);
+    res.status(500).send({
+    message: 'Lỗi server.'
+  });
   }
 })
 
@@ -83,11 +166,12 @@ router.delete("/:id", (req, res) => {
     .catch((err) => res.status(500).send({ message: "Xóa đánh giá thất bại." }));
 });
 
-router.put("/allow-visible", async (req, res) => {
+router.put("/allow-visible/:id", async (req, res) => {
   try{
+    const productId = req.params.id;
     const ids = req.body.idArr;
-    await EvaluateModel.updateMany({_id: {$in: ids}}, {allowVisible: true});
-    await EvaluateModel.updateMany({_id: {$nin: ids}}, {allowVisible: false});
+    await EvaluateModel.updateMany({_id: {$in: ids}, product: productId}, {allowVisible: true});
+    await EvaluateModel.updateMany({_id: {$nin: ids}, product: productId}, {allowVisible: false});
     res.status(200).send({ message: "Cập nhật các đánh giá sẽ được xuất hiện thành công." }) 
   }catch(err){
     res.status(500).send({ message: "Cập nhật các đánh giá sẽ được xuất hiện thất bại." }) 
